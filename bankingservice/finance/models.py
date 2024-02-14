2e46ebcbe84d932bc946523d0e51c920d2b15c11
NITRO <edmondkoko777@gmail.com>
2024-02-14 19:44:42 +0300

    add serializers

--- bankingservice/finance/models.py
+++ bankingservice/finance/models.py
@@ -6,9 +6,15 @@ class Account(models.Model):
     balance = models.DecimalField(max_digits=10, decimal_places=2)
     is_blocked = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.name
+
 
 class Transaction(models.Model):
     sender = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='sent_transactions')
     receiver = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='received_transactions')
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.sender} to {self.receiver} for {self.amount}'
--- bankingservice/finance/serializers.py
+++ bankingservice/finance/serializers.py
@@ -10,6 +10,11 @@ class AccountSerializer(serializers.ModelSerializer):
 
 
 class TransactionSerializer(serializers.ModelSerializer):
+    # StringRelatedField() используется для представления связанных объектов sender и receiver в виде строк.
+    # По умолчанию, StringRelatedField() возвращает значение метода __str__() модели
+    sender = serializers.StringRelatedField()
+    receiver = serializers.StringRelatedField()
+
     class Meta:
         model = Transaction
         fields = '__all__'
