fe3b64345c67439c6ed863740a75b76b413bd7a5
NITRO <edmondkoko777@gmail.com>
2024-02-14 19:25:49 +0300

    add views

--- bankingservice/finance/views.py
+++ bankingservice/finance/views.py
@@ -1,5 +1,5 @@
 from rest_framework.response import Response
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.decorators import action
 
 from .models import Account, Transaction
@@ -30,3 +30,29 @@ class AccountViewSet(viewsets.ModelViewSet):
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
+    # при создании счета, баланс автоматически устанавливается равным 0 благодаря параметру default=0 в модели Account.
+    # При проведении транзакции, проверяется существование отправителя и получателя, а также достаточность баланса у
+    # отправителя. Если все проверки проходят успешно,
+    # балансы счетов обновляются, и транзакция сохраняется в базе данных
+    def create(self, request, *args, **kwargs):
+        sender_id = request.data.get('sender')
+        receiver_id = request.data.get('receiver')
+        amount = request.data.get('amount')
+
+        try:
+            sender = Account.objects.get(id=sender_id)
+            receiver = Account.objects.get(id=receiver_id)
+        except Account.DoesNotExist:
+            return Response({"error": "Отправитель или получатель не существует."},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        if sender.balance < amount:
+            return Response({"error": "Недостаточный баланс у отправителя."},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        sender.balance -= amount
+        receiver.balance += amount
+        sender.save()
+        receiver.save()
+
+        return super().create(request, *args, **kwargs)
